ed731eac347842da006d761b4b54e7f6e9cbf4d3
brendanoco1911 <bkoconnor@smu.edu>
2017-08-06 20:11:18 -0400

    grabbing functionality

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -8,11 +8,9 @@
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+	
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -20,18 +18,104 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-    UE_LOG(LogTemp, Warning, TEXT("GRABBER reporting"));
+    FindPhysicsHandleComponent();
+    SetupInputComponent();
+    
+    
+    
 
 	// ...
 	
 }
 
+void UGrabber::FindPhysicsHandleComponent()
+{
+    // look for attached physics handle
+    PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+    if(PhysicsHandle)
+    {
+        
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName());
+    }
+
+}
+
+void UGrabber::SetupInputComponent()
+{
+    // look for attached input component (appears at run time
+    InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+    if(InputComponent)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Input Component found"));
+        // bind the input axis
+        InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+        InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s missing input component"), *GetOwner()->GetName());
+    }
+}
+
+
+
+void UGrabber::Grab()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+    
+    //LINE TRACE and reach any actors with physics body collision channel set
+    auto HitResult = GetFirstPhysicsBodyInReach();
+    auto ComponentToGrab = HitResult.GetComponent();
+    auto ActorHit = HitResult.GetActor();
+    
+    if(ActorHit)
+    {
+        //If we hit something, attach a physics handle
+        PhysicsHandle->GrabComponentAtLocationWithRotation(
+            ComponentToGrab,
+            NAME_None,
+            ComponentToGrab->GetOwner()->GetActorLocation(),
+            FRotator(0));
+    }
+}
+
+void UGrabber::Release()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Grab released"));
+    
+    //release physics handle
+    PhysicsHandle->ReleaseComponent();
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    //Get player view point this frame/tick
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+                                                               OUT PlayerViewPointLocation,
+                                                               OUT PlayerViewPointRotation);
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+    
+    //if the physics handle is attached
+    if(PhysicsHandle->GrabbedComponent)
+    {
+        //move the object that we are holding
+        PhysicsHandle->SetTargetLocation(LineTraceEnd);
+    }
 
+
+	// ...
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
     FVector PlayerViewPointLocation;
     FRotator PlayerViewPointRotation;
     //Get player view point this frame/tick
@@ -42,25 +126,18 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
     //Draw a red trace in the world to visualize
     
     FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-    DrawDebugLine(GetWorld(),
-                 PlayerViewPointLocation,
-                  LineTraceEnd,
-                  FColor(255, 0, 0),
-                  false,
-                  0.f,
-                  0.f,
-                  10.f);
+    
     
     //setup query parameters
     FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
-//    UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+    //    UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
     //ray cast out to specified reach distance
     FHitResult Hit;
     GetWorld()->LineTraceSingleByObjectType(
-        OUT Hit,
-        PlayerViewPointLocation,
-        LineTraceEnd,
-        FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+                                            OUT Hit,
+                                            PlayerViewPointLocation,
+                                            LineTraceEnd,
+                                            FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
                                             TraceParameters);
     
     //capture what is hit
@@ -69,6 +146,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
     {
         UE_LOG(LogTemp, Warning, TEXT("Object: %s"), *(ActorHit->GetName()));
     }
-	// ...
+    
+    return Hit;
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -26,5 +28,16 @@ public:
 private:
     float Reach = 100.f;
 		
-	
+    UPhysicsHandleComponent* PhysicsHandle = nullptr;
+    UInputComponent* InputComponent = nullptr;
+    
+    // Ray-cast and grab what is in reach
+    void Grab();
+    void Release();
+    
+    void FindPhysicsHandleComponent();
+    void SetupInputComponent();
+    
+    //return hit for first physics body in reach
+    const FHitResult GetFirstPhysicsBodyInReach();
 };
